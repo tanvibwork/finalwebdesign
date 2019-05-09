338332ac54b5ca88a7585f28d531818eed21286d
tanvibwork <50221548+tanvibwork@users.noreply.github.com>
2019-05-09 15:04:18 -0400

    Add files via upload

---
+++ webdesign final/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('#nav').localScroll(800);
+
+	
+	$('#intro').parallax("50%", 0.1);
+	$('#second').parallax("50%", 0.1);
+	$('.bg').parallax("50%", 0.4);
+	$('#third').parallax("50%", 0.3);
+
+})
